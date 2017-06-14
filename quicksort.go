957c338e6022cf6ed8b5f924903ba5c8a85b4f75
spoa <spoa.jp@gmail.com>
2017-06-14 17:17:30 +0900

    add sample programs

---
+++ quicksort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func quicksort(values []int) []int {
+  if len(values) <= 1 { return values }
+
+  low    := []int{}
+  high   := []int{}
+  equal  := []int{}
+
+  pivot  := values[0]
+
+  for _, val := range values {
+    if val > pivot {
+      high = append(high, val)
+    } else if val < pivot {
+      low = append(low, val)
+    } else {
+      equal = append(equal, val)
+    }
+  }
+  return append(append(quicksort(low), equal...), quicksort(high)...)
+}
+
+func main() {
+  fmt.Println(quicksort([]int{ 4, 3, 5, 6, 3, 2, 1 }))
+}
